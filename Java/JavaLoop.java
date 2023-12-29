f55adb321ffa742e76f386ae923ea7e9bb088a20
Mhd Ulil Abshar <87646428+uldevjs@users.noreply.github.com>
2023-12-29 14:21:14 +0700

    update

---
+++ Java/JavaLoop.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.text.*;
+import java.util.Scanner;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+
+import java.util.zip.CRC32;
+
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+
+public class JavaLoop {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+          int   N = sc.nextInt();
+
+        for (int i= 1  ;i<=10;  i++){
+            
+          int result =N*i;
+          System.out.println( N + " x " + i +" = " + result);
+        }
+
+        sc.close();
+    }
+}
---
+++ SQL/RevisingtheSelectQueryII.sql
@@ -0,0 +1,6 @@
+-- Problem	Submissions	Leaderboard	Discussions	Editorial
+-- Query the NAME field for all American cities in the CITY table with populations larger than 120000. The CountryCode for America is USA.
+SELECT NAME
+FROM CITY
+WHERE CountryCode = 'USA'
+AND Population > 120000;
