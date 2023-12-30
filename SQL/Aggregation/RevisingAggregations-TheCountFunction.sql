43052b0a2e90905358f04a9a065886e633ff8d19
Mhd Ulil Abshar <87646428+uldevjs@users.noreply.github.com>
2023-12-30 14:38:50 +0700

    sql

---
+++ SQL/Aggregation/RevisingAggregations-TheCountFunction.sql
@@ -0,0 +1,3 @@
+-- Query a count of the number of cities in CITY having a Population larger than .
+
+SELECT COUNT(Name) FROM CITY WHERE Population > 100000
---
+++ SQL/Aggregation/RevisingAggregations-TheSumFunction.sql
@@ -0,0 +1,2 @@
+-- Query the total population of all cities in CITY where District is California.
+SELECT SUM(Population) FROM CITY WHERE District = 'California'
